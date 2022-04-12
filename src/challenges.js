ab51afc981b79d595d1d162b2aa6cb39646d51b4
Solon <amaral.solon@hotmail.com>
2022-04-12 17:29:06 -0300

    Concluido desafio 5 :fire:

--- src/challenges.js
+++ src/challenges.js
@@ -31,9 +31,12 @@ function concatName(param) {
 console.log(concatName(['foguete', 'não', 'tem', 'ré']));
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let vitorias = (wins * 3)
+  let empates = (ties * 1)
+  return vitorias + empates
 }
+console.log(footballPoints(0, 0))
 
 // Desafio 6
 function highestCount() {
